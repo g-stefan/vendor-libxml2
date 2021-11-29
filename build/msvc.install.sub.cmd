6ade6b7219972d8ac9c981f6478700f70f0f15ab
Grigore Stefan <g_stefan@yahoo.com>
2021-11-29 14:44:23 +0200

    Update

--- build/msvc.install.sub.cmd
+++ build/msvc.install.sub.cmd
@@ -11,9 +11,9 @@ if errorlevel 1 exit 1
 rem ---
 
 if not exist "%INSTALL_PATH_BIN%\" mkdir "%INSTALL_PATH_BIN%"
-copy /Y /B "temp\bin\libxml2.dll" "%INSTALL_PATH_BIN%\libxml2.dll"
-copy /Y /B "temp\bin\xmlcatalog.exe" "%INSTALL_PATH_BIN%\xmlcatalog.exe"
-copy /Y /B "temp\bin\xmllint.exe" "%INSTALL_PATH_BIN%\xmllint.exe"
+copy /Y /B "output\bin\libxml2.dll" "%INSTALL_PATH_BIN%\libxml2.dll"
+copy /Y /B "output\bin\xmlcatalog.exe" "%INSTALL_PATH_BIN%\xmlcatalog.exe"
+copy /Y /B "output\bin\xmllint.exe" "%INSTALL_PATH_BIN%\xmllint.exe"
 
 rem --- dev
 
@@ -21,11 +21,11 @@ if not exist "%INSTALL_PATH_DEV%\" mkdir "%INSTALL_PATH_DEV%"
 if not exist "%INSTALL_PATH_DEV%\bin" mkdir "%INSTALL_PATH_DEV%\bin"
 if not exist "%INSTALL_PATH_DEV%\include" mkdir "%INSTALL_PATH_DEV%\include"
 if not exist "%INSTALL_PATH_DEV%\lib" mkdir "%INSTALL_PATH_DEV%\lib"
-copy /Y /B "temp\bin\libxml2.dll" "%INSTALL_PATH_DEV%\bin\libxml2.dll"
-copy /Y /B "temp\bin\xmlcatalog.exe" "%INSTALL_PATH_DEV%\bin\xmlcatalog.exe"
-copy /Y /B "temp\bin\xmllint.exe" "%INSTALL_PATH_DEV%\bin\xmllint.exe"
-xcopy /Y /S /E "temp\include\libxml2\libxml\" "%INSTALL_PATH_DEV%\include\libxml\"
-copy /Y /B "temp\lib\libxml2.lib" "%INSTALL_PATH_DEV%\lib\libxml2.lib"
-copy /Y /B "temp\lib\libxml2_a.lib" "%INSTALL_PATH_DEV%\lib\libxml2_a.lib"
-copy /Y /B "temp\lib\libxml2_a_dll.lib" "%INSTALL_PATH_DEV%\lib\libxml2_a_dll.lib"
-copy /Y /B "temp\lib\libxml2.lib" "%INSTALL_PATH_DEV%\lib\libxml2.static.lib"
+copy /Y /B "output\bin\libxml2.dll" "%INSTALL_PATH_DEV%\bin\libxml2.dll"
+copy /Y /B "output\bin\xmlcatalog.exe" "%INSTALL_PATH_DEV%\bin\xmlcatalog.exe"
+copy /Y /B "output\bin\xmllint.exe" "%INSTALL_PATH_DEV%\bin\xmllint.exe"
+xcopy /Y /S /E "output\include\libxml2\libxml\" "%INSTALL_PATH_DEV%\include\libxml\"
+copy /Y /B "output\lib\libxml2.lib" "%INSTALL_PATH_DEV%\lib\libxml2.lib"
+copy /Y /B "output\lib\libxml2_a.lib" "%INSTALL_PATH_DEV%\lib\libxml2_a.lib"
+copy /Y /B "output\lib\libxml2_a_dll.lib" "%INSTALL_PATH_DEV%\lib\libxml2_a_dll.lib"
+copy /Y /B "output\lib\libxml2.lib" "%INSTALL_PATH_DEV%\lib\libxml2.static.lib"
