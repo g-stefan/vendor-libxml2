0aa534791913e19bb4bb5a417223dff8d9177d62
Grigore Stefan <g_stefan@yahoo.com>
2024-08-28 17:07:25 +0300

    Update

--- fabricare/make.js
+++ fabricare/make.js
@@ -21,51 +21,52 @@ Shell.mkdirRecursivelyIfNotExists("output/bin");
 Shell.mkdirRecursivelyIfNotExists("output/include");
 Shell.mkdirRecursivelyIfNotExists("output/lib");
 Shell.mkdirRecursivelyIfNotExists("temp");
-Shell.mkdirRecursivelyIfNotExists("temp/bin");
-Shell.mkdirRecursivelyIfNotExists("temp/include");
-Shell.mkdirRecursivelyIfNotExists("temp/lib");
 
 Shell.mkdirRecursivelyIfNotExists("temp/cmake");
 
 if (!Shell.fileExists("temp/build.config.flag")) {
+
 	Shell.setenv("CC", "cl.exe");
 	Shell.setenv("CXX", "cl.exe");
 
-	cmdConfig = "cscript configure.js";
-	cmdConfig += " zlib=yes";
-	cmdConfig += " vcmanifest=yes";
-	cmdConfig += " debug=no";
-	cmdConfig += " static=no";
-	cmdConfig += " xml_debug=yes";
-	cmdConfig += " bindir=" + Shell.realPath(Shell.getcwd()) + "\\temp\\bin";
-	cmdConfig += " incdir=" + Shell.realPath(Shell.getcwd()) + "\\temp\\include";
-	cmdConfig += " libdir=" + Shell.realPath(Shell.getcwd()) + "\\temp\\lib";
-	cmdConfig += " sodir=" + Shell.realPath(Shell.getcwd()) + "\\temp\\bin";
+	cmdConfig = "cmake";
+	cmdConfig += " ../../source";
+	cmdConfig += " -G \"Ninja\"";
+	cmdConfig += " -DCMAKE_BUILD_TYPE=Release";
+	cmdConfig += " -DCMAKE_INSTALL_PREFIX=" + Shell.realPath(Shell.getcwd()) + "\\output";
 
-	runInPath("source\\win32", function() {
+	if (Fabricare.isDynamic()) {
+		cmdConfig += " -DBUILD_SHARED_LIBS=ON";
+		cmdConfig += " -DLIBXML2_WITH_DEBUG=OFF";
+		cmdConfig += " -DLIBXML2_WITH_PYTHON=OFF";
+		cmdConfig += " -DLIBXML2_WITH_TESTS=OFF";
+		cmdConfig += " -DLIBXML2_WITH_LZMA=OFF";
+	};
+
+	if (Fabricare.isStatic()) {
+		cmdConfig += " -DBUILD_SHARED_LIBS=OFF";
+		cmdConfig += " -DLIBXML2_WITH_DEBUG=OFF";
+		cmdConfig += " -DLIBXML2_WITH_PYTHON=OFF";
+		cmdConfig += " -DLIBXML2_WITH_TESTS=OFF";
+		cmdConfig += " -DWIN32_MT_BUILD=ON";
+		cmdConfig += " -DLIBXML2_WITH_LZMA=OFF";
+	};
+
+	runInPath("temp/cmake", function() {
 		exitIf(Shell.system(cmdConfig));
 	});
 
 	Shell.filePutContents("temp/build.config.flag", "done");
 };
 
-runInPath("source\\win32", function() {
-	exitIf(Shell.system("nmake /f Makefile.msvc"));
-	exitIf(Shell.system("nmake /f Makefile.msvc install"));
-	exitIf(Shell.system("nmake /f Makefile.msvc clean"));
+runInPath("temp/cmake", function() {
+	exitIf(Shell.system("ninja"));
+	exitIf(Shell.system("ninja install"));
+	exitIf(Shell.system("ninja clean"));
 });
 
-Shell.system("del /F /Q temp\\bin\\test*");
-Shell.system("del /F /Q temp\\bin\\run*");
-
-exitIf(!Shell.copyFile("temp/bin/libxml2.dll", "output/bin/libxml2.dll"));
-exitIf(!Shell.copyFile("temp/bin/xmlcatalog.exe", "output/bin/xmlcatalog.exe"));
-exitIf(!Shell.copyFile("temp/bin/xmllint.exe", "output/bin/xmllint.exe"));
-
-exitIf(!Shell.copyDirRecursively("temp/include", "output/include"));
-exitIf(!Shell.copyDirRecursively("temp/include/libxml2", "output/include"));
-exitIf(!Shell.copyDirRecursively("temp/lib", "output/lib"));
-
-exitIf(!Shell.copyFile("temp/lib/libxml2.lib", "output/lib/libxml2.static.lib"));
+if (Fabricare.isStatic()) {
+	exitIf(!Shell.copyFile("output/lib/libxml2s.lib", "output/lib/libxml2.lib"));
+};
 
 Shell.filePutContents("temp/build.done.flag", "done");
